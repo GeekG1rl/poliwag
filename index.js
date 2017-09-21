0014e6469244a59b6a36b27a9d22fde53e89f395
mara <mara.wanot@ft.com>
2017-09-21 12:33:44 +0100

    check whether on article page and if scrolled

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 import session from 'next-session-client';
 
-function getUserId () {
-	return session.uuid()
+function getUserId() {
+  return session.uuid()
 		.then(({ uuid }) => uuid);
 }
 
-function sendOffAnEvent () {
-
+function sendOffAnEvent() {
+  console.log('all working ✌️');
 	//		look up this persons data ID
 	//		download their data
 	//		add an event to it
 	//		push it back up
+}
+const setInt = window.setInterval(onArticle, 2000);
 
+// RUN ON PAGE LOADs
+function onArticle() {
+  const ifContent = window.location.href.indexOf('content') !== -1; // check if this is an article page
+  const scrolledDownEnough = window.scrollY > 2000; // if yes check they've read it enough (with scroll position)
+  if (ifContent && scrolledDownEnough) {
+    sendOffAnEvent(); // when yes sendOffAnEvent()
+    clearInterval(setInt);
+  }
 }
 
-
-// RUN ON PAGE LOAD
-
-// 	check if this is an article page
-// 	if yes
-//		check they've read it enough (with scroll position)
-//	when yes
-//		sendOffAnEvent()
