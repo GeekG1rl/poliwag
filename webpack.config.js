8a8a983a518c79c0f7402680cad4552d5e0890ef
Laurie Boyes <laurie.boyes@ft.com>
2017-09-21 16:57:32 +0100

    tiny bit of styling

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin([
     { from: 'manifest.json', to: 'bundle/manifest.json' },
-    { from: 'main.css', to: 'bundle/main.css' },
+    { from: 'main.css', to: 'bundle/main.css' }
     ]),
   ],
 };
