9de29f781182257448e7cfcc9f5b43f2ecf34e2f
Laurie Boyes <laurie.boyes@ft.com>
2017-09-21 14:14:32 +0100

    split up pages

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-import session from 'next-session-client';
-import addEvent from './lib/add-event';
+import articlePage from './lib/pages/article';
+import myFtPage from './lib/pages/myft';
 
-const userIdPromise = session.uuid().then(({ uuid }) => uuid);
+const ifContent = window.location.href.indexOf('content') !== -1; // check if this is an article page
+const ifMyFT = window.location.href.indexOf('myft/following') !== -1; // check if this is a myFT page
 
-function sendOffAnEvent () {
-  return userIdPromise
-    .then(userId => addEvent(userId, {
-      type: 'article-read',
-      articleId: window.location.href.split('/')[window.location.href.split('/').length - 1]
-    }))
-    .then(() => console.log('Saved event ✌️'))
-    .catch(err => console.log('Error saving event 👎', err))
+if(ifContent) {
+  articlePage();
 }
 
-const setInt = window.setInterval(onArticle, 2000);
-
-// RUN ON PAGE LOADs
-function onArticle () {
-  const ifContent = window.location.href.indexOf('content') !== -1; // check if this is an article page
-  const scrolledDownEnough = window.scrollY > 2000; // if yes check they've read it enough (with scroll position)
-  if (ifContent && scrolledDownEnough) {
-    sendOffAnEvent(); // when yes sendOffAnEvent()
-    clearInterval(setInt);
-  }
+else if(ifMyFT) {
+  myFtPage();
 }
