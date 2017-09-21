d974a0b2f4e78873baebffd08ce4cebb5aa1a358
mara <mara.wanot@ft.com>
2017-09-21 16:04:49 +0100

    handlebarsing

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	entry: './index.js',
-	resolve: {
-		modules: ['bower_components', 'node_modules'],
-		descriptionFiles: ["bower.json"]
-	},
-	output: {
-		filename: 'bundle/bundle.js'
-	},
-	plugins: [
-		new CopyWebpackPlugin([
-			{ from: 'manifest.json', to: 'bundle/manifest.json' }
-		])
-	]
-}
+  entry: './index.js',
+  resolve: {
+    modules: ['bower_components', 'node_modules'],
+    descriptionFiles: ['bower.json'],
+  },
+  output: {
+    filename: 'bundle/bundle.js',
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+    { from: 'manifest.json', to: 'bundle/manifest.json' },
+    { from: 'main.css', to: 'bundle/main.css' },
+    ]),
+  ],
+};
