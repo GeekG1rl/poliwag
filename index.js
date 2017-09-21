51e0effd6d648514eec1042be5b25d720a220b18
Laurie Boyes <laurie.boyes@ft.com>
2017-09-21 13:02:23 +0100

    editorconfig so we don't fight

--- index.js
+++ index.js
@@ -1,72 +1,72 @@
 import session from 'next-session-client';
 
 const urls = {
-	newJson: 'https://jsonblob.com/api/jsonBlob',
-	userIndex: 'https://jsonblob.com/api/jsonBlob/20a13269-9eba-11e7-aa97-49ae4ae22db0'
+  newJson: 'https://jsonblob.com/api/jsonBlob',
+  userIndex: 'https://jsonblob.com/api/jsonBlob/20a13269-9eba-11e7-aa97-49ae4ae22db0'
 }
 
 function getUserId () {
-	return session.uuid()
-		.then(({ uuid }) => uuid);
+  return session.uuid()
+    .then(({ uuid }) => uuid);
 }
 
 function getJsonId () {
-	let userId;
-	return Promise.all([
-		getUserId(),
-		fetch(urls.userIndex)
-			.then(res => res.json())
-	])
-		.then(([userId, userIndex]) => {
-			if (userIndex[userId]) {
-				return userIndex[userId];
-			} else {
-				return fetch(urls.newJson, {
-					method: 'POST',
-					body: JSON.stringify({
-						events: []
-					}),
-					headers: {
-						'Content-Type': 'application/json',
-						'Accept': 'application/json'
-					}
-				})
-					.then(res => {
-						const jsonId = res.headers.get('Location').split('/')[res.headers.get('Location').split('/').length - 1];
+  let userId;
+  return Promise.all([
+    getUserId(),
+    fetch(urls.userIndex)
+      .then(res => res.json())
+  ])
+    .then(([userId, userIndex]) => {
+      if (userIndex[userId]) {
+        return userIndex[userId];
+      } else {
+        return fetch(urls.newJson, {
+          method: 'POST',
+          body: JSON.stringify({
+            events: []
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          }
+        })
+          .then(res => {
+            const jsonId = res.headers.get('Location').split('/')[res.headers.get('Location').split('/').length - 1];
 
-						// save the jsonId
-						userIndex[userId] = jsonId;
-						return fetch(urls.userIndex, {
-							method: 'PUT',
-							body: JSON.stringify(userIndex),
-							headers: {
-								'Content-Type': 'application/json',
-								'Accept': 'application/json'
-							}
-						}).then(() => {
-							return jsonId;
-						})
-					});
-			}
-		});
+            // save the jsonId
+            userIndex[userId] = jsonId;
+            return fetch(urls.userIndex, {
+              method: 'PUT',
+              body: JSON.stringify(userIndex),
+              headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+              }
+            }).then(() => {
+              return jsonId;
+            })
+          });
+      }
+    });
 }
 
-function getUserId() {
+function getUserId () {
   return session.uuid()
-		.then(({ uuid }) => uuid);
+    .then(({ uuid }) => uuid);
 }
 
-function sendOffAnEvent() {
+function sendOffAnEvent () {
   console.log('all working ✌️');
-	//		look up this persons data ID
-	//		download their data
-	//		add an event to it
-	//		push it back up
+  //		look up this persons data ID
+  //		download their data
+  //		add an event to it
+  //		push it back up
 }
 const setInt = window.setInterval(onArticle, 2000);
 
 // RUN ON PAGE LOADs
-function onArticle() {
+function onArticle () {
   const ifContent = window.location.href.indexOf('content') !== -1; // check if this is an article page
   const scrolledDownEnough = window.scrollY > 2000; // if yes check they've read it enough (with scroll position)
   if (ifContent && scrolledDownEnough) {
