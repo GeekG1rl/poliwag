ac40f2a44631827530271db6b05d4365f0939068
Laurie Boyes <laurie.boyes@ft.com>
2017-09-21 10:55:10 +0100

    some psuedocode

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('go team');
+
+function sendOffAnEvent() {
+
+//		look up this persons data ID
+//		download their data
+//		add an event to it
+//		push it back up
+
+}
+
+
+// RUN ON PAGE LOAD
+
+// 	check if this is an article page
+// 	if yes
+//		check they've read it enough (with scroll position)
+//	when yes
+//		sendOffAnEvent()
