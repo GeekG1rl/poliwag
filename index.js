9ae0af28e4c1a0a362e17c6bdbabd081605e98b7
Laurie Boyes <laurie.boyes@ft.com>
2017-09-22 10:00:31 +0100

    homepage banner

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import articlePage from './lib/pages/article';
 import myFtPage from './lib/pages/myft';
+import homePage from './lib/pages/homepage';
 
 const ifContent = window.location.href.indexOf('content') !== -1; // check if this is an article page
 const ifMyFT = window.location.href.indexOf('myft/following') !== -1; // check if this is a myFT page
+const isHomepage = window.location.href.match(/ft.com\/$/) // check if this is the homepage
 
 if (ifContent) {
   articlePage();
 } else if (ifMyFT) {
   myFtPage();
+} else if (isHomepage) {
+  homePage();
 }
