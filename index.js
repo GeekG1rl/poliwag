28a8d09e9b04f440435e04d2d6358d9a2eca02b3
Laurie Boyes <laurie.boyes@ft.com>
2017-09-22 10:17:39 +0100

    award page progress

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import articlePage from './lib/pages/article';
 import myFtPage from './lib/pages/myft';
+import myFtExplorePage from './lib/pages/myft/explore';
 import homePage from './lib/pages/homepage';
 
 const ifContent = window.location.href.indexOf('content') !== -1; // check if this is an article page
 const ifMyFT = window.location.href.indexOf('myft/following') !== -1; // check if this is a myFT page
+const ifMyFTExplore = window.location.href.indexOf('myft/explore') !== -1; // check if this is a myFT page
 const isHomepage = window.location.href.match(/ft.com\/$/) // check if this is the homepage
 
 if (ifContent) {
   articlePage();
 } else if (ifMyFT) {
   myFtPage();
+} else if (ifMyFTExplore) {
+  myFtExplorePage();
 } else if (isHomepage) {
   homePage();
 }
